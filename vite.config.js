ed44a8b339e0a5a297e10b3773cddff368ee98d9
GloFlav <rasgloflav@gmail.com>
2025-05-02 17:58:37 +0300

    Fix: missing dialog component import

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from "path"
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   server: {
     host: true,
-    allowedHosts: ['6da2-129-222-108-22.ngrok-free.app']
+    allowedHosts: [
+      "6da2-129-222-108-22.ngrok-free.app",
+      "hellosoins.com",
+      "preprod.hellosoins.com",
+    ],
   },
-  host: '0.0.0.0',
+  host: "0.0.0.0",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   base: "/medicalReact",
-})
+});
