dc173d5475fb9702ae5f026418883d79f8be1d43
GloFlav <rasgloflav@gmail.com>
2025-05-05 10:37:36 +0300

    fix another majuscule nenteee

--- src/pages/praticien/ProfileComponents/GestionDesTarif.jsx
+++ src/pages/praticien/ProfileComponents/GestionDesTarif.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Card, CardContent, CardHeader } from '@/components/ui/Card';
 import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import { Avatar } from '@/components/ui/Avatar';
