edaa847195ba59430f1db1ae9f52ded74f026346
GloFlav <rasgloflav@gmail.com>
2025-05-06 09:45:53 +0300

    fix import

--- src/pages/praticien/ProfileComponents/TroubleEtSolution.jsx
+++ src/pages/praticien/ProfileComponents/TroubleEtSolution.jsx
@@ -1,6 +1,6 @@
 // TroubleManager.jsx
 import React, { useState } from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/Dialog";
 import { Button } from "@/components/ui/Button";
 import { Loader2 } from 'lucide-react';
 import TableList from './TroubleEtSolution';
